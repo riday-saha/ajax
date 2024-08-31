c2455bd41d8d619017b890c391cfa57f1500c3ee
Riday Saha <sahariday33@gmail.com>
2024-08-31 10:20:52 +0600

    Update dropdown.blade.php

--- resources/views/dropdown.blade.php
+++ resources/views/dropdown.blade.php
@@ -5,7 +5,7 @@
     <meta name="csrf-token" content="content">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel AJAX Dependent Country State City Dropdown Example - ItSolutionStuff.com</title>
+    <title>Laravel AJAX Dependent Country State City Dropdown Example </title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
